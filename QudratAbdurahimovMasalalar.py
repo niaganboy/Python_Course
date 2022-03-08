fc6926bd31f51310dbab96beb7853a04dc934567
Doston <100999265+niaganboy@users.noreply.github.com>
2022-03-08 14:34:35 +0500

    Update QudratAbdurahimovMasalalar.py
    Xatolar tuzatildi!
    Bug fixes!

--- QudratAbdurahimovMasalalar.py
+++ QudratAbdurahimovMasalalar.py
@@ -1,10 +1,10 @@
 print("Kvadratning tomoni a berilgan.\nUning perimetrini hisoblang")
 P = "4 * a"
-print(f"Perimetr = " + P)
+print(f"Betis stadion Perimetr = " + P)
 print("Kvadratning tomoni a berilgan.\nUning yuzasini hisoblang")
 print("S = a ** 2")
 print("To'g'ri to'rtburchakning tomonlari a va b berilgan.Uning yuzasi va perimetri aniqlansin")
-a = 19
-b = 11
-print(f"Perimetri: {(a + b) * 2} ")
-print(f"Yuzasi: {a * b}")
+a = "Xuanmi"
+b = "Betis"
+#print(f"Perimetri: {(a + b) * 2} ")
+#print(f"Yuzasi: {a * b}")
